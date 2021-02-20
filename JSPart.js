e49e5e7e879623e81cfee438b5cf8ac03a65d4fe
LoterPlanel <LoterPlanel@github.com>
2021-02-21 00:41:30 +0200

    all

--- JSPart.js
+++ JSPart.js
@@ -1,4 +1,4 @@
-var LIST	=	$('.bl-list');
+/*var LIST	=	$('.bl-list');
 
 var ITEM_TEMPLATE	=	$('.bl-row').html();
 
@@ -11,4 +11,41 @@ function	addItem(title)
 	node.remove();
 	});
 	LIST.append(node);	//Add	to	the	end	of	the	list
-}
+}*/
+
+const Item = (text) => `
+	<div class="bl-row">
+		<div class="bl-product">
+			${text}
+		</div>
+		<div class="bl-count">
+			<button class="bl-minus" data-tooltip="minus">-</>-</button>
+			<span class="bl-num">1</span>
+			<button class="bl-plus" data-tooltip="plus">+</button>
+		</div>
+		<div class="bl-del">
+			<button class="bl-buttons" data-tooltip="Bought">Купленo</button> <button class="bl-buttons bl-delbut" data-tooltip="Delete"><b><FONT COLOR=white>x</FONT></b></button>
+		</div>
+	</div>
+`;
+
+const ItemRight = (text) => `
+	<div class="bl-prod">${text}<div class="bl-prod-num">1</div></div>
+`;
+
+const addNewItem = $('button[data-tooltip="Add"]');
+addNewItem.click(() => {
+	const input = $('#productNameInput');
+	const productName = input.val();
+	if (!productName) return;
+	const list = $('#productList');
+	list.append(Item(productName));
+	const listRight = $('#remainingProduct');
+	listRight.append(ItemRight(productName));
+	input.val("");
+});
+
+const plus = $('button[data-tooltip="plus"]');
+plus.click( () => {
+	const input = 
+});
